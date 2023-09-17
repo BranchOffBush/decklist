18b58e710d60fa6273f81c56d6299caac2481c9f
BranchOffBush <johnny115115@gmail.com>
2023-09-16 20:25:16 -0700

    this was just for practice

--- Documents/bored/decks.py
+++ Documents/bored/decks.py
@@ -11,27 +11,27 @@ def preGen1(topDocString, bottomDocString, divider):
     overview = "(old standard deck of amonkhet)"
     innerDocString = "same concept as above but now you can goryos on t3"
     decklist = """Deck
-    2 Verdurous Gearhulk (KLD) 172
-    4 Canyon Slough (AKH) 239
-    3 Cataclysmic Gearhulk (KLD) 9
-    4 Cathartic Reunion (KLD) 109
-    4 Combustible Gearhulk (KLD) 112
-    1 Concealed Courtyard (KLD) 245
-    2 Cut // Ribbons (AKH) 223
-    4 Dragonskull Summit (XLN) 252
-    3 Evolving Wilds (AKH) 242
-    3 Fatal Push (FNM) 93
-    3 God-Pharaoh's Gift (HOU) 161
-    2 Inspiring Vantage (KLD) 246
-    4 Madcap Experiment (KLD) 122
-    3 Mountain (XLN) 272
-    4 Noxious Gearhulk (KLD) 96
-    3 Plains (XLN) 260
-    4 Refurbish (KLD) 25
-    2 Swamp (XLN) 268
-    2 Sweltering Suns (AKH) 149
-    3 Tormenting Voice (AKH) 151
-    """
+2 Verdurous Gearhulk (KLD) 172
+4 Canyon Slough (AKH) 239
+3 Cataclysmic Gearhulk (KLD) 9
+4 Cathartic Reunion (KLD) 109
+4 Combustible Gearhulk (KLD) 112
+1 Concealed Courtyard (KLD) 245
+2 Cut // Ribbons (AKH) 223
+4 Dragonskull Summit (XLN) 252
+3 Evolving Wilds (AKH) 242
+3 Fatal Push (FNM) 93
+3 God-Pharaoh's Gift (HOU) 161
+2 Inspiring Vantage (KLD) 246
+4 Madcap Experiment (KLD) 122
+3 Mountain (XLN) 272
+4 Noxious Gearhulk (KLD) 96
+3 Plains (XLN) 260
+4 Refurbish (KLD) 25
+2 Swamp (XLN) 268
+2 Sweltering Suns (AKH) 149
+3 Tormenting Voice (AKH) 151
+"""
     print(overview)
     print(topDocString)
     print(innerDocString)
@@ -48,44 +48,44 @@ def preGen2(topDocString, bottomDocString, divider):
     overview = "(historic recreation of said standard deck, but with a goryo's twitst (priest of fell rites))"
     innerDocString = "same concept as above but now you can goryos on t3"
     decklist = """Deck
-    2 Unburial Rites (ISD) 122
-    1 Swamp (ANA) 5
-    1 Sheoldred, Whispering One (NPH) 73
-    4 Priest of Fell Rites (MH2) 208
-    1 Plains (ANA) 1
-    2 Cathartic Reunion (KLR) 116
-    4 Faithless Looting (STA) 38
-    2 Cataclysmic Gearhulk (KLR) 12
-    2 Refurbish (KLR) 27
-    2 Noxious Gearhulk (KLR) 104
-    3 Combustible Gearhulk (KLR) 120
-    2 Mountain (ANA) 7
-    4 Godless Shrine (RNA) 248
-    2 Concealed Courtyard (KLR) 282
-    4 Blood Crypt (RNA) 245
-    2 Furycalm Snarl (STX) 266
-    4 Sacred Foundry (GRN) 254
-    1 Griselbrand (SIS) 31
-    2 Late to Dinner (MH2) 19
-    1 Elesh Norn, Grand Cenobite (NPH) 9
-    2 Olivia, Crimson Bride (VOW) 245
-    2 Cathartic Pyre (MID) 133
-    3 Bone Shards (MH2) 76
-    2 Etali, Primal Conqueror (MOM) 137
-    2 Portal to Phyrexia (BRO) 240
-    1 Blackcleave Cliffs (ONE) 248
-    2 The Balrog, Durin's Bane (LTR) 195
+2 Unburial Rites (ISD) 122
+1 Swamp (ANA) 5
+1 Sheoldred, Whispering One (NPH) 73
+4 Priest of Fell Rites (MH2) 208
+1 Plains (ANA) 1
+2 Cathartic Reunion (KLR) 116
+4 Faithless Looting (STA) 38
+2 Cataclysmic Gearhulk (KLR) 12
+2 Refurbish (KLR) 27
+2 Noxious Gearhulk (KLR) 104
+3 Combustible Gearhulk (KLR) 120
+2 Mountain (ANA) 7
+4 Godless Shrine (RNA) 248
+2 Concealed Courtyard (KLR) 282
+4 Blood Crypt (RNA) 245
+2 Furycalm Snarl (STX) 266
+4 Sacred Foundry (GRN) 254
+1 Griselbrand (SIS) 31
+2 Late to Dinner (MH2) 19
+1 Elesh Norn, Grand Cenobite (NPH) 9
+2 Olivia, Crimson Bride (VOW) 245
+2 Cathartic Pyre (MID) 133
+3 Bone Shards (MH2) 76
+2 Etali, Primal Conqueror (MOM) 137
+2 Portal to Phyrexia (BRO) 240
+1 Blackcleave Cliffs (ONE) 248
+2 The Balrog, Durin's Bane (LTR) 195
 
-    Sideboard
-    1 Velomachus Lorehold (STX) 245
-    1 Ilharg, the Raze-Boar (WAR) 133
-    2 Serra's Emissary (MH2) 30
-    1 Skithiryx, the Blight Dragon (MUL) 82
-    2 Urabrask the Hidden (MUL) 88
-    4 Fragmentize (KLR) 18
-    2 Kolaghan's Command (DTK) 224
-    2 Fracture (STX) 188
-    """
+Sideboard
+1 Velomachus Lorehold (STX) 245
+1 Ilharg, the Raze-Boar (WAR) 133
+2 Serra's Emissary (MH2) 30
+1 Skithiryx, the Blight Dragon (MUL) 82
+2 Urabrask the Hidden (MUL) 88
+4 Fragmentize (KLR) 18
+2 Kolaghan's Command (DTK) 224
+2 Fracture (STX) 188
+"""
     print(overview)
     print(topDocString)
     print(innerDocString)
@@ -101,37 +101,37 @@ def preGen3(topDocString, bottomDocString, divider):
     overview = "(historic mizzex breach mixed with goryos twist and a phyreixan subtheme)"
     innerDocString = "goryo's t3, mizzex + breach = value"
     decklist = """Deck
-    2 Thrilling Discovery (STX) 243
-    4 Mizzix's Mastery (STA) 43
-    2 Rise of the Dark Realms (JMP) 271
-    1 Swamp (UST) 214
-    4 Faithless Looting (STA) 38
-    3 Cathartic Reunion (KLR) 116
-    3 Cathartic Pyre (MID) 133
-    4 Priest of Fell Rites (MH2) 208
-    2 Unburial Rites (ISD) 122
-    1 Elesh Norn, Grand Cenobite (NPH) 9
-    1 Elesh Norn (MOM) 12
-    1 Vorinclex, Monstrous Raider (KHM) 199
-    4 Breach the Multiverse (MOM) 94
-    1 Sheoldred, Whispering One (NPH) 73
-    1 Etali, Primal Conqueror (MOM) 137
-    1 Urabrask the Hidden (MUL) 88
-    1 Urabrask (MOM) 169
-    1 Zopandrel, Hunger Dominus (ONE) 195
-    1 Vorinclex, Voice of Hunger (MUL) 94
-    1 Plains (UST) 212
-    1 Mountain (UST) 215
-    4 Blood Crypt (RNA) 245
-    4 Godless Shrine (RNA) 248
-    1 Concealed Courtyard (KLR) 282
-    1 Blackcleave Cliffs (ONE) 248
-    4 Sacred Foundry (GRN) 254
-    1 Inspiring Vantage (KLR) 283
-    2 Savai Triome (IKO) 253
-    2 Stitcher's Supplier (M19) 121
-    1 Scholar of the Lost Trove (JMP) 14
-    """
+2 Thrilling Discovery (STX) 243
+4 Mizzix's Mastery (STA) 43
+2 Rise of the Dark Realms (JMP) 271
+1 Swamp (UST) 214
+4 Faithless Looting (STA) 38
+3 Cathartic Reunion (KLR) 116
+3 Cathartic Pyre (MID) 133
+4 Priest of Fell Rites (MH2) 208
+2 Unburial Rites (ISD) 122
+1 Elesh Norn, Grand Cenobite (NPH) 9
+1 Elesh Norn (MOM) 12
+1 Vorinclex, Monstrous Raider (KHM) 199
+4 Breach the Multiverse (MOM) 94
+1 Sheoldred, Whispering One (NPH) 73
+1 Etali, Primal Conqueror (MOM) 137
+1 Urabrask the Hidden (MUL) 88
+1 Urabrask (MOM) 169
+1 Zopandrel, Hunger Dominus (ONE) 195
+1 Vorinclex, Voice of Hunger (MUL) 94
+1 Plains (UST) 212
+1 Mountain (UST) 215
+4 Blood Crypt (RNA) 245
+4 Godless Shrine (RNA) 248
+1 Concealed Courtyard (KLR) 282
+1 Blackcleave Cliffs (ONE) 248
+4 Sacred Foundry (GRN) 254
+1 Inspiring Vantage (KLR) 283
+2 Savai Triome (IKO) 253
+2 Stitcher's Supplier (M19) 121
+1 Scholar of the Lost Trove (JMP) 14
+"""
     print(overview)
     print(topDocString)
     print(innerDocString)
@@ -147,38 +147,38 @@ def preGen4(topDocString, bottomDocString, divider):
     overview = "(historic has just become a monstrosity, like why?)"
     innerDocString = "swaped: Stitcher's Supplier for gingerbrute, Thrilling Discovery plus a couple other cards for The Witch's Vanity + Beseech the mirror, unburial rites for late to dinner (more food for mirror)"
     decklist = """Deck
-    1 Late to Dinner (MH2) 19
-    4 Mizzix's Mastery (STA) 43
-    1 Rise of the Dark Realms (JMP) 271
-    1 Swamp (UST) 214
-    4 Faithless Looting (STA) 38
-    3 Cathartic Reunion (KLR) 116
-    2 Cathartic Pyre (MID) 133
-    4 Priest of Fell Rites (MH2) 208
-    2 The Witch's Vanity (WOE) 119
-    1 Elesh Norn, Grand Cenobite (NPH) 9
-    1 Elesh Norn (MOM) 12
-    1 Vorinclex, Monstrous Raider (KHM) 199
-    4 Breach the Multiverse (MOM) 94
-    1 Sheoldred, Whispering One (NPH) 73
-    1 Etali, Primal Conqueror (MOM) 137
-    1 Urabrask the Hidden (MUL) 88
-    1 Urabrask (MOM) 169
-    1 Zopandrel, Hunger Dominus (ONE) 195
-    1 Vorinclex, Voice of Hunger (MUL) 94
-    1 Plains (UST) 212
-    1 Mountain (UST) 215
-    4 Blood Crypt (RNA) 245
-    4 Godless Shrine (RNA) 248
-    1 Concealed Courtyard (KLR) 282
-    1 Blackcleave Cliffs (ONE) 248
-    4 Sacred Foundry (GRN) 254
-    1 Inspiring Vantage (KLR) 283
-    2 Savai Triome (IKO) 253
-    2 Gingerbrute (ELD) 219
-    1 Scholar of the Lost Trove (JMP) 14
-    3 Beseech the Mirror (WOE) 82
-    """
+1 Late to Dinner (MH2) 19
+4 Mizzix's Mastery (STA) 43
+1 Rise of the Dark Realms (JMP) 271
+1 Swamp (UST) 214
+4 Faithless Looting (STA) 38
+3 Cathartic Reunion (KLR) 116
+2 Cathartic Pyre (MID) 133
+4 Priest of Fell Rites (MH2) 208
+2 The Witch's Vanity (WOE) 119
+1 Elesh Norn, Grand Cenobite (NPH) 9
+1 Elesh Norn (MOM) 12
+1 Vorinclex, Monstrous Raider (KHM) 199
+4 Breach the Multiverse (MOM) 94
+1 Sheoldred, Whispering One (NPH) 73
+1 Etali, Primal Conqueror (MOM) 137
+1 Urabrask the Hidden (MUL) 88
+1 Urabrask (MOM) 169
+1 Zopandrel, Hunger Dominus (ONE) 195
+1 Vorinclex, Voice of Hunger (MUL) 94
+1 Plains (UST) 212
+1 Mountain (UST) 215
+4 Blood Crypt (RNA) 245
+4 Godless Shrine (RNA) 248
+1 Concealed Courtyard (KLR) 282
+1 Blackcleave Cliffs (ONE) 248
+4 Sacred Foundry (GRN) 254
+1 Inspiring Vantage (KLR) 283
+2 Savai Triome (IKO) 253
+2 Gingerbrute (ELD) 219
+1 Scholar of the Lost Trove (JMP) 14
+3 Beseech the Mirror (WOE) 82
+"""
     print(overview)
     print(topDocString)
     print(innerDocString)
@@ -194,37 +194,37 @@ def preGen5(topDocString, bottomDocString, divider):
     overview = "(historic 5c beanstalk elementals)"
     innerDocString = "this is literally just stupid...I have nothing else to say about this one. Have not made sideboard for bo3"
     decklist = """Companion
-    1 Jegantha, the Wellspring (IKO) 222
+1 Jegantha, the Wellspring (IKO) 222
 
-    Deck
-    4 Up the Beanstalk (WOE) 195
-    4 Omnath, Locus of All (MOM) 249
-    4 Risen Reef (M20) 217
-    4 Mulldrifter (J21) 38
-    4 Explore (JMP) 393
-    2 Nissa, Resurgent Animist (MAT) 22
-    1 Jegantha, the Wellspring (IKO) 222
-    3 Brushfire Elemental (ZNR) 221
-    3 Omnath, Locus of the Roil (M20) 216
-    1 A-Omnath, Locus of Creation (ZNR) 232
-    1 Illuna, Apex of Wishes (IKO) 190
-    2 Vadrok, Apex of Thunder (IKO) 214
-    3 Healer of the Glade (M20) 176
-    4 Unclaimed Territory (XLN) 258
-    4 Secluded Courtyard (NEO) 275
-    1 Island (UST) 213
-    1 Mountain (UST) 215
-    1 Forest (UST) 216
-    1 Steam Vents (GRN) 257
-    4 Stomping Ground (RNA) 259
-    4 Breeding Pool (RNA) 246
-    2 Yarok, the Desecrated (M20) 220
-    1 Overgrown Tomb (GRN) 253
-    1 Temple Garden (GRN) 258
+Deck
+4 Up the Beanstalk (WOE) 195
+4 Omnath, Locus of All (MOM) 249
+4 Risen Reef (M20) 217
+4 Mulldrifter (J21) 38
+4 Explore (JMP) 393
+2 Nissa, Resurgent Animist (MAT) 22
+1 Jegantha, the Wellspring (IKO) 222
+3 Brushfire Elemental (ZNR) 221
+3 Omnath, Locus of the Roil (M20) 216
+1 A-Omnath, Locus of Creation (ZNR) 232
+1 Illuna, Apex of Wishes (IKO) 190
+2 Vadrok, Apex of Thunder (IKO) 214
+3 Healer of the Glade (M20) 176
+4 Unclaimed Territory (XLN) 258
+4 Secluded Courtyard (NEO) 275
+1 Island (UST) 213
+1 Mountain (UST) 215
+1 Forest (UST) 216
+1 Steam Vents (GRN) 257
+4 Stomping Ground (RNA) 259
+4 Breeding Pool (RNA) 246
+2 Yarok, the Desecrated (M20) 220
+1 Overgrown Tomb (GRN) 253
+1 Temple Garden (GRN) 258
 
-    Sideboard
-    1 Jegantha, the Wellspring (IKO) 222
-    """
+Sideboard
+1 Jegantha, the Wellspring (IKO) 222
+"""
     print(overview)
     print(topDocString)
     print(innerDocString)
